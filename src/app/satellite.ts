0b68b0417fa0d1d510475a728bbf0e05375bd64f
Vanessa <vangalepark@gmail.com>
2019-12-13 22:06:58 -0600

    Added Satellite class and new satellite objects

---
+++ src/app/satellite.ts
@@ -0,0 +1,17 @@
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name:string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
+}
